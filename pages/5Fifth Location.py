c19d022b5892b2a178e28e71fa059f4521c22ad6
EL <m4gpi3girl@gmail.com>
2024-03-15 23:36:29 +0000

    Update 5Fifth Location.py

--- pages/5Fifth Location.py
+++ pages/5Fifth Location.py
@@ -16,3 +16,5 @@ bb = st.text_input("Complete the number plate: KS62 '____'")
 if bb == password1 or bb == password2 or bb == 'Drz':
 
   st.write("### WELL DONE! MESSAGE REESE THE WORD 'DOOMSDAY' FOR FURTHER INSTRUCTION")
+
+  st.markdown("<iframe src="https://giphy.com/embed/rIdj5s9Ead7osqcuih" width="480" height="412" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/friends-butterfly-bees-rIdj5s9Ead7osqcuih">via GIPHY</a></p>")
